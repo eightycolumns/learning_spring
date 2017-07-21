8e15ea925a01d1c17deae00ca38c70c437f0365f
Drew Keller <drew@eightycolumns.com>
2017-07-21 19:17:06 -0400

    Register a shutdown hook

--- src/main/java/com/eightycolumns/application/Application.java
+++ src/main/java/com/eightycolumns/application/Application.java
@@ -1,18 +1,17 @@
 package com.eightycolumns.application;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
   public static void main(String[] args) {
-    ApplicationContext ac = new ClassPathXmlApplicationContext(
+    ConfigurableApplicationContext ac = new ClassPathXmlApplicationContext(
       "application.xml"
     );
 
     Instrument oboe = ac.getBean("oboe", Oboe.class);
     Musician oboist = ac.getBean("oboist", Musician.class);
 
-    ((ConfigurableApplicationContext)ac).close();
+    ac.registerShutdownHook();
   }
 }
